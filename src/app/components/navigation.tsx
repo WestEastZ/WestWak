ef222e61cd7e95e4e5ca2a380bab5c5c8c4b0f77
WestEastZ <tjehdwn10110@naver.com>
2024-05-11 15:57:11 +0900

    💄 [Style] Navbar

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import Menu from "./menu";
+
+export default function Navigation() {
+  const [ismenuOpen, setIsmenuOpen] = useState<boolean>(false);
+
+  const handleMenuToggle = () => {
+    setIsmenuOpen(!ismenuOpen);
+  };
+
+  return (
+    <>
+      <nav className="sticky z-[9990] w-full p-5 text-3xl  font-bold text-white bg-bgColor-100">
+        <section className="flex justify-between items-center max-w-[80rem] m-auto">
+          <div className="flex justify-center items-center text-4xl">
+            WESTWAK
+          </div>
+          <div
+            onClick={handleMenuToggle}
+            className={`menuTrigger ${ismenuOpen ? "active" : ""}`}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </section>
+      </nav>
+      <Menu ismenuOpen={ismenuOpen} />
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,41 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: {
+          100: "#1C1D1F",
+        },
+        characterColor: {
+          wak: "#1CBC74",
+          ine: "#8A2BE2",
+          jingburger: "#F0A957",
+          lilpa: "#3330db",
+          jururu: "#FF008C",
+          gosegu: "#467EC6",
+          vlichan: "#95C100",
+        },
+      },
+      fontFamily: {
+        spoqa: ["SpoqaHanSansNeo", "sans-serif"],
+      },
+      // keyframes: {
+      //   slideDown: {
+      //     "0%": { transform: "translateY(0)" },
+      //     "50%": { transform: "translateY(125%)" },
+      //     "70%": { transform: "translateY(120%)" },
+      //     "100%": { transform: "translateY(125%)" },
+      //   },
+      //   slideUp: {
+      //     "0%": { transform: "translateY(125%)" },
+      //     "100%": { transform: "translateY(0%)" },
+      //   },
+      // },
+      // animation: {
+      //   "slide-down": "slideDown 0.8s ease-in-out forwards",
+      //   "slide-up": "slideUp 0.3s ease-in-out forwards",
+      // },
+    },
   },
   plugins: [],
 };
