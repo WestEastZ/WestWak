1e31677d4c9aba4a33ab572d6590825370f3f9a6
WestEastZ <tjehdwn10110@naver.com>
2024-06-24 16:03:11 +0900

    ✨ Components [Title, Button, Input, Footer, Nav]

---
+++ src/app/_components/input/Input.tsx
@@ -0,0 +1,19 @@
+import { InputPropsType } from "@/app/_types/type";
+import React from "react";
+
+export default function Input({
+  type,
+  name,
+  placeholder,
+  onChange,
+}: InputPropsType) {
+  return (
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      className="w-full h-14 p-2 bg-inherit border-b-2 focus:border-bgColor-main outline-none"
+    />
+  );
+}
---
+++ src/app/_containers/footer/footer.tsx
@@ -0,0 +1,71 @@
+import { MemberList } from "@/app/_constants/MemberList";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#F3F3F3]">
+      <section className="w-[80rem] m-auto pt-10 flex justify-between text-base font-bold">
+        {MemberList.map((member) => (
+          <section key={member.name} className="flex flex-col gap-2">
+            <div className="uppercase text-center">{member.name}</div>
+            <div className="flex flex-col gap-1">
+              {member.items.map((item) => (
+                <Link
+                  key={item}
+                  href={"/"}
+                  className="font-medium transform duration-300 hover:scale-110 text-center"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </section>
+        ))}
+      </section>
+      <section className="w-[80rem] m-auto pt-10 pb-10 flex flex-col gap-1 text-3xl font-bold text-right">
+        <div>WAKVIDEO</div>
+        <div className="relative text-base font-medium">
+          왁비디오와 함께 최신 영상을 만나보세요
+        </div>
+        <div className="flex gap-1 justify-end">
+          <Link href={"https://cafe.naver.com/steamindiegame"}>
+            <Image
+              src={"/image/cafe.webp"}
+              alt="cafe"
+              width={36}
+              height={36}
+              className="transform duration-300 hover:scale-110"
+            />
+          </Link>
+          <Link href={"https://www.instagram.com/instawakgood/?hl=ko"}>
+            <Image
+              src={"/image/instagram.png"}
+              alt="cafe"
+              width={36}
+              height={36}
+              className="transform duration-300 hover:scale-110"
+            />
+          </Link>
+          <Link href={"https://bj.afreecatv.com/ecvhao"}>
+            <Image
+              src={"/image/tv.png"}
+              alt="cafe"
+              width={36}
+              height={36}
+              className="transform duration-300 hover:scale-110"
+            />
+          </Link>
+        </div>
+        <Link href={"/developer"} className="text-base font-medium">
+          {"개발자에게 한마디 하러 가기 >"}
+        </Link>
+      </section>
+      <section className="p-3 bg-bgColor-100 text-white">
+        <div className="w-[80rem] m-auto font-medium text-sm">
+          COPYRIGHT ⓒ WAKVIDEO. ALL RIGHT RESERVED
+        </div>
+      </section>
+    </footer>
+  );
+}
