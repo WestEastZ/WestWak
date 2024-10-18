64b5053d774c20f9fc76debcf8faecbe49476bb8
WestEastZ <tjehdwn10110@naver.com>
2024-10-18 12:30:34 +0900

    🎨 [Improve] Improve Code

--- src/app/_components/common/Swiper/SwiperCompoenent.tsx
+++ src/app/_components/common/Swiper/SwiperCompoenent.tsx
@@ -5,59 +5,169 @@ import {
   Autoplay,
   Navigation,
   Scrollbar,
+  Pagination,
   EffectCoverflow,
 } from "swiper/modules";
 import SwiperCore from "swiper";
 import type { Swiper as SwiperType } from "swiper/types";
+import type { SwiperOptions } from "swiper/types";
 
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 import "swiper/css/effect-fade";
 
 import Image from "next/image";
 
-export default function SwiperComponent({
+type ObjectFit = "contain" | "cover" | "fill" | "none" | "scale-down";
+
+interface SwiperImageSizeType {
+  width: number;
+  height: number;
+  objectFit?: ObjectFit;
+}
+
+interface SlideType {
+  imageUrl: string;
+  title: string;
+  description: string;
+}
+
+interface SwiperOptionType<T extends string | SlideType> {
+  slides: T[];
+  swiperOptions?: SwiperOptions;
+  loop?: boolean;
+  autoplay?:
+    | boolean
+    | {
+        delay: number;
+        disableOnInteraction: boolean;
+        reverseDirection: boolean;
+      };
+  autoplayDirection?: boolean;
+  speed?: number;
+  direction?: "horizontal" | "vertical";
+  showNavigation?: boolean;
+  showPagination?: boolean;
+  showScrollbar?: boolean;
+  imageSize?: SwiperImageSizeType;
+  navigationButtonsStyle?: React.CSSProperties;
+  paginationButtonsStyle?: React.CSSProperties;
+  containerStyle?: React.CSSProperties;
+  swiperType?: T extends SlideType ? "home" : string;
+}
+
+export default function SwiperComponent<T extends string | SlideType>({
   slides,
-  children,
-}: {
-  slides: any[];
-  children?: React.ReactNode;
-}) {
-  SwiperCore.use([Autoplay]);
+  swiperOptions = {}, // spaceBetween, slidesPreview
+  loop = false,
+  autoplay = true,
+  autoplayDirection = false,
+  speed = 3000,
+  direction = "horizontal",
+  showNavigation = false,
+  showPagination = false,
+  showScrollbar = false,
+  imageSize = { width: 100, height: 100, objectFit: "cover" },
+  navigationButtonsStyle = {},
+  paginationButtonsStyle = {},
+  containerStyle = {},
+  swiperType,
+}: SwiperOptionType<T>) {
+  SwiperCore.use([
+    Navigation,
+    Scrollbar,
+    Pagination,
+    Autoplay,
+    EffectCoverflow,
+  ]);
+
   const swiperRef = useRef<SwiperType>();
-  const swiperOption = {
-    module: [Navigation, Scrollbar, Autoplay, EffectCoverflow],
+
+  const defaultSwiperOption = {
+    modules: [Navigation, Scrollbar, Pagination, Autoplay, EffectCoverflow],
     spaceBetween: 5,
     slidesPerView: 10,
-    loop: true,
-    autoplay: {
-      delay: 0,
-      pauseOnMouseEnter: false,
-      disableOnInteraction: false,
-    },
-    speed: 3000,
+    centeredSlides: swiperOptions.centeredSlides
+      ? swiperOptions.centeredSlides
+      : false,
+    loop: loop ? true : false,
+    autoplay: autoplay
+      ? {
+          delay: 0,
+          disableOnInteraction: false,
+          reverseDirection: autoplayDirection,
+        }
+      : false,
+    speed: speed ? speed : 0,
+    direction: direction,
+    navigation: showNavigation,
+    pagination: showPagination
+      ? {
+          clickable: true,
+          el: ".swiper-pagination",
+          type: "bullets" as const,
+          renderBullet: function (index: number, className: string) {
+            return '<span class="' + className + '">' + "</span>";
+          },
+        }
+      : false,
+    scrollbar: showScrollbar ? { draggable: true } : false,
     onSwiper: (swiper: SwiperType) => {
       swiperRef.current = swiper;
     },
   };
 
+  const mergeOption = { ...defaultSwiperOption, ...swiperOptions };
+
+  const swiperStyle = {
+    height: direction === "vertical" ? `100vh` : `${imageSize.height}px`,
+    ...containerStyle,
+  };
+
   return (
-    <div className="w-full container-style">
-      <Swiper {...swiperOption} className="swiper-wrapper">
+    <div
+      className="relative flex flex-col items-center gap-4"
+      style={swiperStyle}
+    >
+      <Swiper {...mergeOption} className="swiper-wrapper">
         {slides.map((slide) => (
-          <SwiperSlide key={slide}>
-            <div className="w-[100px] h-[100px] box-style overflow-hidden">
-              <img
-                src={slide}
-                alt="image"
-                className="w-full h-full object-cover"
+          <SwiperSlide key={typeof slide === "string" ? slide : slide.imageUrl}>
+            <div
+              className={`box-style group relative overflow-hidden rounded-2xl border border-customColor-border`}
+              style={{
+                width: `${imageSize.width}px`,
+                height: `${imageSize.height}px`,
+              }}
+            >
+              <Image
+                src={typeof slide === "string" ? slide : slide.imageUrl}
+                alt={typeof slide === "string" ? `image` : slide.title}
+                fill
+                sizes={"500px"}
+                priority
+                style={{ objectFit: imageSize.objectFit }}
               />
+
+              {swiperType === "home" && (
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 duration-300 group-hover:bg-opacity-70">
+                  <div className="flex flex-col gap-2 text-center font-bold opacity-0 duration-300 group-hover:opacity-100">
+                    <span className="text-2xl text-customColor-main">
+                      {typeof slide === "string" ? slide : slide.title}
+                    </span>
+                    <span className="text-lg text-white">
+                      {typeof slide === "string" ? slide : slide.description}
+                    </span>
+                  </div>
+                </div>
+              )}
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      {showPagination && <div className="swiper-pagination"></div>}
     </div>
   );
 }
--- src/app/_components/common/modal/ModalWakfaceResult.tsx
+++ src/app/_components/common/modal/ModalWakfaceResult.tsx
@@ -6,36 +6,110 @@ import LoadingSnail from "../loading/LoadingSnail";
 
 export default function ModalWakfaceResult({
   result,
+  resultArr,
 }: {
   result: ResultType | undefined;
+  resultArr: ResultType[] | undefined;
 }) {
-  const [percent, setPercent] = useState<string | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (result) {
-      const percent = result.distance * 100;
-      const fixed = percent.toFixed(2);
-      setPercent(fixed);
-    }
-
-    const timer = setTimeout(() => setIsLoading(false), 10000);
+    const timer = setTimeout(() => setIsLoading(false), 3000);
 
     return () => clearTimeout(timer);
-  }, [result]);
+  }, [resultArr]);
+
+  const reorderedResults = resultArr
+    ? [resultArr[1], resultArr[0], resultArr[2]]
+    : [];
 
   return (
-    <div className="text-white flex justify-center">
-      {isLoading ? (
+    <div className="flex items-center justify-center h-full">
+      {isLoading || !resultArr ? (
         <LoadingSnail />
       ) : (
-        <div className="flex flex-col gap-4 text-center">
-          <span className="text-customColor-main text-3xl font-bold">{`${percent}%`}</span>
-
-          <img src={result?.blob} width={250} />
-          <span className="text-2xl font-bold">{result?.fileName}</span>
+        <div className="flex justify-evenly w-full h-full p-4 gap-4 text-center box-style">
+          {reorderedResults.map((item, index) => (
+            <React.Fragment key={item.fileName}>
+              {index !== 0 && (
+                <div className="w-px h-full bg-customColor-border my-2"></div>
+              )}
+              <WakfaceResultItem
+                key={item.fileName}
+                result={item}
+                index={index}
+              />
+            </React.Fragment>
+          ))}
         </div>
       )}
+    </div>
+  );
+}
+
+function WakfaceResultItem({
+  result,
+  index,
+}: {
+  result: ResultType;
+  index: number;
+}) {
+  const [count, setCount] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+
+  useEffect(() => {
+    const duration = 2000;
+    const steps = 30;
+    const increment = result.distance / steps;
+    let current = 0;
+
+    const timer = setInterval(() => {
+      current += increment;
+      if (current > result.distance) {
+        clearInterval(timer);
+        current = result.distance;
+      }
+      setCount(current);
+      setHeight(current);
+    }, duration / steps);
+
+    return () => clearInterval(timer);
+  }, [result.distance]);
+
+  return (
+    <div className="relative flex flex-col gap-2 justify-center items-center text-white">
+      {/* chart */}
+      <section className="flex justify-center items-end w-8 h-full rounded-full bg-customColor-border">
+        <div
+          className={`relative w-full rounded-full transition-all duration-2000 ${
+            index === 1 ? "bg-customColor-main" : "bg-customColor-light_box"
+          }`}
+          style={{ height: `${height + 5}%` }}
+        >
+          <div
+            className={`absolute -top-4 right-1/2  w-12 h-12 transform translate-x-1/2  rounded-full border-4 ${
+              index === 1 ? "bg-customColor-main" : "bg-customColor-light_box"
+            }`}
+            style={{
+              boxShadow: "-4px 0 6px rgba(0, 0, 0, 0.2)",
+              transition: "all 2000ms ease-out",
+            }}
+          >
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg font-bold">
+              <span>{`${count.toFixed(0)}`}</span>
+              <span className="text-sm">{`%`}</span>
+            </span>
+          </div>
+        </div>
+      </section>
+
+      {/* imgae */}
+      <section className="w-[100px] h-[100px] flex-shrink-0">
+        <img
+          src={result.blob}
+          className="w-full h-full bg-customColor-dark_box rounded-2xl object-cover transition-transform duration-2000"
+        />
+      </section>
     </div>
   );
 }
