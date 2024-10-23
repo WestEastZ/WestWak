60d76ae751a082b684577265c12a2bb14b8d5f33
WestEastZ <tjehdwn10110@naver.com>
2024-10-23 20:42:11 +0900

    🎨 [Improve] Improve Iamge Import

--- src/app/(page)/info/_components/AlbumCover.tsx
+++ src/app/(page)/info/_components/AlbumCover.tsx
@@ -1,28 +1,27 @@
+"use client";
+
 import Image, { StaticImageData } from "next/image";
-import React from "react";
-import KiddingAlbum from "../../../../../public/image/KIDDING.jpg";
-import RewindAlbum from "../../../../../public/image/RE:WIND.jpg";
-import woowak from "../../../../../public/image/woowak.webp";
+import React, { useMemo } from "react";
 import { InformationType } from "@/app/_types/type";
 
-export default function AlbumCover({ data }: { data: InformationType }) {
-  const title = data.title;
-  let albumCover: StaticImageData;
+import KiddingAlbum from "../../../../../public/image/KIDDING.jpg";
+import RewindAlbum from "../../../../../public/image/RE:WIND.jpg";
+import WoowakImage from "../../../../../public/image/woowak.webp";
 
-  switch (title) {
-    case "KIDDING":
-      albumCover = KiddingAlbum;
-      break;
-    case "RE:WIND":
-      albumCover = RewindAlbum;
-      break;
-    default:
-      albumCover = woowak;
-      break;
-  }
+const ALBUM_COVERS = {
+  KIDDING: KiddingAlbum,
+  "RE:WIND": RewindAlbum,
+  DEFAULT: WoowakImage,
+} as const;
+
+export default function AlbumCover({ data }: { data: InformationType }) {
+  const albumCover = useMemo(() => {
+    const key = data.title as keyof typeof ALBUM_COVERS;
+    return ALBUM_COVERS[key] || ALBUM_COVERS.DEFAULT;
+  }, [data.title]);
 
   return (
-    <div className="flex flex-col gap-4 rounded-full overflow-hidden">
+    <div className="flex flex-col gap-4 overflow-hidden rounded-full">
       <Image src={albumCover} alt={`${albumCover}`} width={100} priority />
     </div>
   );
