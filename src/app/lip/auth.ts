23d53fbfcef2ba563e6e5b3d294e27e1ef75a3f0
WestEastZ <tjehdwn10110@naver.com>
2024-11-09 15:19:28 +0900

    🎨 [Improve] Middleware Header / Cookie

--- src/app/lip/auth.ts
+++ src/app/lip/auth.ts
@@ -29,7 +29,7 @@ export const authenticate = async () => {
   });
 
   const statusCode = response.status;
-  const data = response.json();
+  const data = await response.json();
   return { statusCode, data };
 };
 
@@ -46,63 +46,65 @@ export const getNewAccessToken = async () => {
     },
   });
 
-  return response.json();
+  const statusCode = response.status;
+  const data = await response.json();
+  return { statusCode, data };
 };
 
 // response header access 토큰 설정
-export function setResponseHeader(response: NextResponse, token: string) {
-  response.cookies.set("access_token", `${token}`, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none" as const,
-    maxAge: 10000,
-    domain: process.env.NODE_ENV === "development" ? "" : ".wakvideo.shop",
-    path: "/",
-  });
+// export function setResponseHeader(response: NextResponse, token: string) {
+//   response.cookies.set("access_token", `${token}`, {
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: "none" as const,
+//     maxAge: 10000,
+//     domain: process.env.NODE_ENV === "development" ? "" : ".wakvideo.shop",
+//     path: "/",
+//   });
 
-  return response;
-}
+//   return response;
+// }
 
 // request header 설정
-export function setRequestHeader(
-  request: NextRequest,
-  response: NextResponse,
-  newToken?: string,
-) {
-  const token = newToken || request.cookies.get("access_token")?.value;
+// export function setRequestHeader(
+//   request: NextRequest,
+//   response: NextResponse,
+//   newToken?: string,
+// ) {
+//   const token = newToken || request.cookies.get("access_token")?.value;
 
-  if (token) {
-    const newHeaders = new Headers(request.headers);
+//   if (token) {
+//     const newHeaders = new Headers(request.headers);
 
-    newHeaders.set("Authorization", `${token}`);
+//     newHeaders.set("Authorization", `${token}`);
 
-    const cookieString = response.cookies
-      .getAll()
-      .map((cookie) => `${cookie.name}=${cookie.value}`)
-      .join("; ");
+//     const cookieString = request.cookies
+//       .getAll()
+//       .map((cookie) => `${cookie.name}=${cookie.value}`)
+//       .join("; ");
 
-    if (cookieString) {
-      newHeaders.set("Cookie", cookieString);
-    }
+//     if (cookieString) {
+//       newHeaders.set("Cookie", cookieString);
+//     }
 
-    const newRequest = new NextRequest(request, {
-      headers: newHeaders,
-    });
+//     const newRequest = new NextRequest(request, {
+//       headers: newHeaders,
+//     });
 
-    const newResponse = NextResponse.next({ request: newRequest });
+//     const newResponse = NextResponse.next({ request: newRequest });
 
-    // header 복사
-    response.headers.forEach((value, key) => {
-      newResponse.headers.set(key, value);
-    });
+//     // header 복사
+//     response.headers.forEach((value, key) => {
+//       newResponse.headers.set(key, value);
+//     });
 
-    // cookie 복사
-    response.cookies.getAll().forEach((cookie) => {
-      newResponse.cookies.set(cookie.name, cookie.value, cookie);
-    });
+//     // cookie 복사
+//     response.cookies.getAll().forEach((cookie) => {
+//       newResponse.cookies.set(cookie.name, cookie.value, cookie);
+//     });
 
-    return newResponse;
-  }
+//     return newResponse;
+//   }
 
-  return response;
-}
+//   return response;
+// }
