3a4bdbdfc63416cdeccf81dad97454c9ed9b7c8c
WestEastZ <tjehdwn10110@naver.com>
2024-11-26 16:31:06 +0900

    💄 [Style] Recent Video

--- src/app/_components/common/button/ButtonSlide.tsx
+++ src/app/_components/common/button/ButtonSlide.tsx
@@ -23,7 +23,7 @@ export default function ButtonSlide({
   return (
     <div
       onClick={handleClick}
-      className="group flex h-full cursor-pointer items-center justify-center p-1"
+      className="group flex h-full w-full cursor-pointer items-center justify-center p-1"
     >
       <div className="flex h-[22px] w-[22px] cursor-pointer items-center justify-center">
         {direction === "prev" ? (
